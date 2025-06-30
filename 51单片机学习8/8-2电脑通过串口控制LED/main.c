def34f749af919ee214e04dea762941219709775
1 <1>
2025-06-30 21:41:19 +0800

    51单片机学习8

---
+++ 51单片机学习8/8-2电脑通过串口控制LED/main.c
@@ -0,0 +1,27 @@
+#include <REGX52.H>
+#include "Delay.h"
+#include "UART.h"
+
+
+
+
+void  main()
+{
+	UART_Init();
+
+	while(1)
+	{
+		
+	}
+}
+
+void UATR_Routine() interrupt 4
+{
+	if(RI==1)
+	{
+		P2=~SBUF;
+		UART_SendByte(SBUF);
+		RI=0;
+	}
+	
+}
