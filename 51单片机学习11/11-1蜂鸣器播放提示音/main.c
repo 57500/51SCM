ae465c3d242a773b29dfef0eef269609c9b01d11
57500 <3480087763@qq.com>
2025-07-01 19:35:19 +0800

    Add files via upload

---
+++ 51单片机学习11/11-1蜂鸣器播放提示音/main.c
@@ -0,0 +1,30 @@
+#include <REGX52.H>
+#include "Delay.h"
+#include "Key.h"
+#include "Nixie.h"
+
+
+sbit Buzzer=P2^5;
+
+unsigned char KeyNum;
+unsigned int i;
+
+void  main()
+{
+	Nixie(1,0);
+	while(1)
+	{
+
+		KeyNum=Key();
+		if(KeyNum)
+		{
+			for(i=0;i<100;i++)
+			{
+				Buzzer=!Buzzer;
+				Delay(1);
+				
+			}
+			Nixie(1,KeyNum);
+		}
+	}
+}
